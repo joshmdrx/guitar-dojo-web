5881a29a0823b111ee7a82ec6dd9a93af01fc46d
joshmdrx <mrjoshuajmatthews@gmail.com>
2024-08-25 11:52:10 +0100

    better mobile steps

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.utils.toArray(".step").forEach((step, i) => {
-  gsap.fromTo(
-    step,
-    {
-      z: -300, // Start even farther away to enhance the fly-in effect
-      scale: 0.5, // Start slightly more scaled down for a dramatic effect
-      opacity: 0, // Start with opacity 0 to create a fade-in effect
-      yPercent: 50, // Start lower down the screen
-    },
-    {
-      z: 0, // Bring to the original position
-      scale: 1, // Scale back to normal size
-      opacity: 1, // Fade in to full opacity
-      yPercent: 0, // Move to the correct position on the screen
-      ease: "linear", // Smooth easing for a more natural transition
-      scrollTrigger: {
-        trigger: step,
-        start: "top 90%", // Start when the top of the step is near the bottom of the viewport
-        end: "top 40%", // End when the top of the step is closer to the middle of the viewport
-        scrub: true, // Link the animation to the scroll position for a smooth effect
+// Define different animations for different screen sizes
+gsap.matchMedia().add("(min-width: 769px)", () => {
+  // Animation for desktops and tablets
+  gsap.utils.toArray(".step").forEach((step, i) => {
+    gsap.fromTo(
+      step,
+      {
+        z: -300,
+        scale: 0.7,
+        opacity: 0,
+        yPercent: 50,
       },
-    }
-  );
+      {
+        z: 0,
+        scale: 1,
+        opacity: 1,
+        yPercent: 0,
+        ease: "linear",
+        scrollTrigger: {
+          trigger: step,
+          start: "top 80%",
+          end: "top 30%",
+          scrub: true,
+        },
+      }
+    );
+  });
+});
+
+gsap.matchMedia().add("(max-width: 768px)", () => {
+  gsap.utils.toArray(".step").forEach((step, i) => {
+    gsap.fromTo(
+      step,
+      {
+        z: -150,
+        scale: 0.8,
+        opacity: 0,
+        yPercent: 20,
+      },
+      {
+        z: 0,
+        scale: 1,
+        opacity: 1,
+        yPercent: 0,
+        ease: "linear",
+        scrollTrigger: {
+          trigger: step,
+          start: "top 85%", // Start the animation earlier
+          end: "top 60%", // End the animation later
+          scrub: 1.5, // Create a smoother, slightly delayed transition
+        },
+      }
+    );
+  });
 });
