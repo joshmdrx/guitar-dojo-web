8d87bddba5c921fb1257c01066bf0d52b83bd0f7
joshmdrx <mrjoshuajmatthews@gmail.com>
2024-08-29 20:45:20 +0100

    idk add padding

--- script.js
+++ script.js
@@ -47,9 +47,9 @@ gsap.matchMedia().add("(max-width: 768px)", () => {
         ease: "power2.inOut",
         scrollTrigger: {
           trigger: step,
-          start: "top 120%", // Start the animation earlier
-          end: "top 100%", // End the animation later
-          scrub: 1, // Create a smoother, slightly delayed transition
+          start: "top 100%", // Start the animation earlier
+          end: "top 90%", // End the animation later
+          scrub: 1.5, // Create a smoother, slightly delayed transition
         },
       }
     );
